14c428d21d26e76c2b97a01be74d77d73bb2d097
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-04-08 10:56:03 -0500

    Agregado: configuración de imports alias para el proyecto

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/app': path.resolve(__dirname, './src/modules/app'),
+      '@/auth': path.resolve(__dirname, './src/modules/auth'),
+      '@/brand': path.resolve(__dirname, './src/modules/brand'),
+      '@/categories': path.resolve(__dirname, './src/modules/categories'),
+      '@/products': path.resolve(__dirname, './src/modules/products'),
+      '@/shared': path.resolve(__dirname, './src/modules/shared'),
+    },
+  },
+});
