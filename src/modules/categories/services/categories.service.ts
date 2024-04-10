ab8b187d06c98f210eb42585c41018ccc54f0012
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-04-10 12:35:57 -0500

    Actualizado: servicio de categorías con método para crear una categoría

--- src/modules/categories/services/categories.service.ts
+++ src/modules/categories/services/categories.service.ts
@@ -1,11 +1,12 @@
-import { CategoryRes } from '@/categories/interfaces';
+import { Category } from '@/categories/interfaces';
+import { CategoryForm } from '@/categories/schemas';
 import { http } from '@/shared/config';
 import { isAxiosError } from 'axios';
 
 export class CategoriesService {
-  static getCategories = async (): Promise<CategoryRes[]> => {
+  static getCategories = async (): Promise<Category[]> => {
     try {
-      const { data } = await http<CategoryRes[]>('/categorias');
+      const { data } = await http<Category[]>('/categorias');
       return data;
     } catch (error) {
       if (isAxiosError(error)) {
@@ -14,4 +15,20 @@ export class CategoriesService {
       throw new Error('Ocurrió un error al obtener las categorías');
     }
   };
+
+  static createCategory = async (category: CategoryForm): Promise<string> => {
+    try {
+      const { data } = await http.post<PostResp>('/categorias', category);
+      return data.mensaje;
+    } catch (error) {
+      if (isAxiosError(error)) {
+        throw new Error(error.response?.data.message);
+      }
+      throw new Error('Ocurrió un error al crear la categoría');
+    }
+  };
+}
+
+interface PostResp {
+  mensaje: string;
 }
