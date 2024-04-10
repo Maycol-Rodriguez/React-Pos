03a13cc1e3250d942146ed50c1b34c8bd54603e8
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-04-10 12:36:01 -0500

    Agregado: hooks de consulta y creación de categoría

--- src/modules/categories/hooks/useQueryCategory.ts
+++ src/modules/categories/hooks/useQueryCategory.ts
@@ -1,5 +1,7 @@
+import { CategoryForm } from '@/categories/schemas';
 import { CategoriesService } from '@/categories/services';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 export const useQueryCategory = () => {
   const { data, isLoading, isError } = useQuery({
@@ -8,4 +10,28 @@ export const useQueryCategory = () => {
   });
 
   return { data, isLoading, isError };
+};
+
+export const usePostCategory = () => {
+  const queryClient = useQueryClient();
+  const { isError, mutate } = useMutation({
+    mutationFn: (categoryForm: CategoryForm) =>
+      CategoriesService.createCategory(categoryForm),
+    onMutate: async () => {
+      toast.loading('Creando categoria...');
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ['categories'] });
+      toast.success(data);
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+    onSettled: () => {
+      toast.dismiss();
+    },
+  });
+
+  return { isError, mutate };
 };
