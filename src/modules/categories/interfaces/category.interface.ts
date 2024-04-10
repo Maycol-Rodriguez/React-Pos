3a2ef2462b83c757e02e4f15e20a18fc8458a7ad
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-04-10 12:35:50 -0500

    Actualizado: interfaz de categoría

--- src/modules/categories/interfaces/category.interface.ts
+++ src/modules/categories/interfaces/category.interface.ts
@@ -1,7 +1,8 @@
-export interface CategoryRes {
+export interface Category {
   id: number;
   nombre: string;
   descripcion: string;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
+  actions?: string;
 }
