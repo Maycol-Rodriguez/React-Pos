4d6b79bc98ad952642b2a0025ad6577735dfa8c9
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-04-14 16:34:24 -0500

    Corrección: métodos deleteProduct y updateProduct en ProductService

--- src/modules/products/services/product.service.ts
+++ src/modules/products/services/product.service.ts
@@ -27,4 +27,28 @@ export class ProductsService {
       throw new Error('Ocurrió un error al crear el producto');
     }
   };
+
+  static deleteProduct = async (id: number): Promise<string> => {
+    try {
+      const { data } = await http.delete<ProductResponse>(`/productos/${id}`);
+      return data.mensaje;
+    } catch (error) {
+      if (isAxiosError(error)) {
+        throw new Error(error.response?.data.message);
+      }
+      throw new Error('Ocurrió un error al eliminar el producto');
+    }
+  }
+
+  static updateProduct = async (id: number, product: ProductForm): Promise<string> => {
+    try {
+      const { data } = await http.put<ProductResponse>(`/productos/${id}`, product);
+      return data.mensaje;
+    } catch (error) {
+      if (isAxiosError(error)) {
+        throw new Error(error.response?.data.message);
+      }
+      throw new Error('Ocurrió un error al actualizar el producto');
+    }
+  }
 }
