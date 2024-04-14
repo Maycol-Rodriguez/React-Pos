c1ef9f5c83af8b59d80a099523f7d16d267cca62
Maycol Rodriguez <maycol.rodriguez.ma@gmail.com>
2024-04-14 16:33:51 -0500

    Corregido: Cambio de tipo de dato de precio a number en la interfaz de producto

--- src/modules/products/schemas/ProductSchema.ts
+++ src/modules/products/schemas/ProductSchema.ts
@@ -2,28 +2,26 @@ import { z } from 'zod';
 
 export const ProductSchema = z.object({
   id: z.number().optional(),
-  nombre: z
-    .string()
-    .min(3, 'El nombre del producto debe tener al menos 3 caracteres'),
+  nombre: z.string().min(3, 'El nombre del producto debe tener al menos 3 caracteres'),
   descripcion: z.string().optional(),
-  codigo: z
-    .string()
-    .min(1, 'El código del producto debe tener al menos 1 caracter'),
-  precio: z
+  codigo: z.string().min(3, 'El código del producto debe tener al menos 1 caracter'),
+  precio: z.coerce
     .number()
-    .positive()
+    .positive('Debe ser mayor a 0')
     .min(1, 'El precio del producto debe ser mayor a 0'),
-  stock: z
+  stock: z.coerce
     .number()
-    .positive()
-    .min(1, 'El stock del producto debe ser mayor a 0'),
-  stock_minimo: z
-    .number()
-    .positive()
+    .int('Debe ser un número entero')
+    .positive('Debe ser mayor a 0')
     .min(1, 'El stock mínimo del producto debe ser mayor a 0'),
-  categoria_id: z.number().positive().min(1, 'La categoría es requerida'),
-  marca_id: z.number().positive().min(1, 'La marca es requerida'),
-  unidad_id: z.number().positive().min(1, 'La unidad es requerida'),
+  stock_minimo: z.coerce
+    .number()
+    .int('Debe ser un número entero')
+    .positive('Debe ser mayor a 0')
+    .min(1, 'El stock mínimo es requerida'),
+  categoria_id: z.coerce.number().min(1, 'La categoria es requerida'),
+  marca_id: z.coerce.number().min(1, 'La marca es requerida'),
+  unidad_id: z.coerce.number().min(1, 'La unidad es requerida'),
 });
 
 export type ProductForm = z.infer<typeof ProductSchema>;
